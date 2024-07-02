168a9c761bde9520455fef4263f361ca960ce7e6
Shannz <134748195+Shannzx10@users.noreply.github.com>
2024-07-02 10:05:45 +0700

    update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { thinkany, tudouai, useadrenaline, GoodyAI, luminai, blackbox, CgtAi, Sim
 const { PlayStore, apkcombo, aptoide, BukaLapak, happymod, stickersearch, filmapik21, webtoons, resep, gore, mangatoon, android1, wattpad } = require('./scrape/search');
 const { tiktok, tiktoks, capcut, tiktokAll, ttStalker, ttSlide, instagram, pinterest } = require('./scrape/downloader');
 const { ephoto } = require('./scrape/ephoto');
+const { ssweb } = require('./scrape/tools');
 const config = require('./config');
 const msg = config.messages;
 const app = express();
@@ -110,56 +111,6 @@ async function elxyz(q, sesi, mdl) {
         console.error('Error:', error);
         throw error;
     }
-}
-
-const ssweb = (url, device = 'desktop') => {
-    return new Promise((resolve, reject) => {
-        const base = 'https://www.screenshotmachine.com';
-        const param = {
-            url: url,
-            device: device,
-            cacheLimit: 0
-        };
-
-        axios({
-            url: base + '/capture.php',
-            method: 'POST',
-            data: new URLSearchParams(Object.entries(param)),
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
-            }
-        }).then((data) => {
-            const cookies = data.headers['set-cookie'];
-            if (data.data.status == 'success') {
-                axios.get(base + '/' + data.data.link, {
-                    headers: {
-                        'cookie': cookies.join('')
-                    },
-                    responseType: 'arraybuffer'
-                }).then(({ data }) => {
-                    resolve(data);
-                }).catch(reject);
-            } else {
-                reject(`Link Error: ${data.data.message}`);
-            }
-        }).catch(reject);
-    });
-};
-
-function uuid() {
-    let d = new Date().getTime();
-    let d2 = (performance && performance.now && (performance.now() * 1000)) || 0;
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        let r = Math.random() * 16;
-        if (d > 0) {
-            r = (d + r) % 16 | 0;
-            d = Math.floor(d / 16);
-        } else {
-            r = (d2 + r) % 16 | 0;
-            d2 = Math.floor(d2 / 16);
-        }
-        return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
-    });
 }
 
 app.get('/tools/sswebphone', (req, res) => {
